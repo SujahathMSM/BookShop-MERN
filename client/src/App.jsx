4283f5f9d51d582d23e0c3e7feb55850978b5d7d
sujahathmsm <sujahathmhmd@gmail.com>
2024-06-04 23:50:27 +0530

    Added the get book feature using both client and server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home/Home";
+import Book from "./routes/Books/Book";
 import About from "./routes/About/About";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/books" element={<Book />} />
         </Routes>
         <Footer />
       </Router>
--- server/index.js
+++ server/index.js
@@ -1,49 +1,55 @@
-require('dotenv').config();
-const cors = require('cors');
-const express = require('express');
-const connectionDB = require('./connectDB');
-const connectDB = require('./connectDB');
-const Book = require('./models/Books');
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const connectionDB = require("./connectDB");
+const connectDB = require("./connectDB");
+const Book = require("./models/Books");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 (async () => {
-    try {
-        await connectDB();
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
-    }
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
 })();
 
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
-app.get("/",(req, res) => {
-    res.send("Hello World");
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 //get all books
 app.get("/books", async (req, res) => {
-    try {
-        await Book.find({}).limit(2)
-        .sort({ createdAt: -1 })
-        .then((books) => {
-            res.json(books);
-        })
-        .catch((err) => {
-            console.log(err.message)
-        })
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  try {
+    const category = req.query.category;
+
+    const filter = {};
+
+    if (category) {
+      filter.category = category;
     }
-})
+    const data = await Book.find(filter);
+
+    if (!data) {
+      throw new Error("An error occurred while fetching books.");
+    }
+
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 app.get("*", (req, res) => {
-    res.sendStatus("404");
-})
-
+  res.sendStatus("404");
+});
