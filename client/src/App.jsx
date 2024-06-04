3ceffdda70c060874f1de6dd5f82d031ce81e30c
sujahathmsm <sujahathmhmd@gmail.com>
2024-06-04 16:46:18 +0530

    Constructed the Front End - Partially

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home/Home";
+import About from "./routes/About/About";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/routes/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>A Book Store Website Developed using React JS </h1>
+        <p>Backend: Node JS + Express JS & MongoDB</p>
+    </div>
+  )
+}
+
+export default Home
